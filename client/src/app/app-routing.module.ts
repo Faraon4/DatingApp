d6725819f123d1c6d6e283354d295a569ef0c10a
RaduDarii <radu@stud.uni-obuda.hu>
2021-09-29 15:17:23 +0200

    Make the route to the errors

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -17,11 +18,13 @@ const routes: Routes = [
       {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]}, // We add a route for our members
   {path: 'members/:id', component: MemberDetailComponent}, // we add route for the details of the our members , and for going to details page we will write the id of person
   {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
+  {path: 'messages', component: MessagesComponent}
+    ]
+  },
+  // Route for the errors
+  {path: 'errors', component: TestErrorsComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'}, // we write this if the user is entering something that not exits, this is also called wild route
                                                              // pathMatch: 'full', the URL should be all correct, not only a part of it 
-    ]
-  }
 ];
 
 @NgModule({
