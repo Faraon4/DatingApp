70cf7e4d589fe73a55ca11fb1b5a359e80762761
RaduDarii <radu@stud.uni-obuda.hu>
2021-09-29 03:16:07 +0200

    Making owm controller to get the and handle error

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [Authorize] // User should be authorized
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "Secret Text";
+        }
+
+         
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if( thing is null) return NotFound();
+            return Ok();
+        }
+
+         
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            try
+            {
+   var thing = _context.Users.Find(-1);
+           var thingToReturn = thing.ToString(); // null reference exeption
+
+           return thingToReturn;
+            }
+            catch(Exception ex)
+            {
+               return StatusCode(500, "Computer Says NOOO!!");
+            }
+        }
+
+        
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
