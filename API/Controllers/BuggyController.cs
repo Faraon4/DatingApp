5eff65d2f73c13f29051e66ea77f25d96d14eb0a
RaduDarii <radu@stud.uni-obuda.hu>
2021-09-30 02:02:19 +0200

    Testing by modifying buggyController

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -14,7 +14,7 @@ namespace API.Controllers
         private readonly DataContext _context;
         public BuggyController(DataContext context)
         {
-            _context = context;
+             _context = context;
 
         }
 
@@ -25,27 +25,27 @@ namespace API.Controllers
             return "Secret Text";
         }
 
-         
+
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
             var thing = _context.Users.Find(-1);
-            if( thing is null) return NotFound();
+            if (thing is null) return NotFound();
             return Ok();
         }
 
-         
+
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
-           var thing = _context.Users.Find(-1);
-           var thingToReturn = thing.ToString(); // null reference exeption
+            var thing = _context.Users.Find(-1);
+            var thingToReturn = thing.ToString(); // null reference exeption
 
-           return thingToReturn;
-          
+            return thingToReturn;
+
         }
 
-        
+
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
