8d942905ee110333d2e75c3803fb4e3b845c5567
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-01 01:08:54 +0200

    For working this code, we need to delete the db, and then, make next steps: 1. migrate only AppUser table , but without the collection of photoes, then we add the collection of photoes in appuser and we add the anotation to the table(Photoes) and migrate this, update the db,  then we delete the db and make the program class to be like it is now

--- API/Program.cs
+++ API/Program.cs
@@ -16,23 +16,23 @@ namespace API
     {
         public static async Task Main(string[] args)
         {
-           var host =  CreateHostBuilder(args).Build(); // build instrad the run, and we need to add run back anyway, to run the app
-           using var scope = host.Services.CreateScope(); // We create a scope for the service that we will create
-           var services = scope.ServiceProvider;
-           try
-           {
-               var context = services.GetRequiredService<DataContext>();
-               await context.Database.MigrateAsync(); // MigrateAsync---> We update the database , or create the database
-                                                      // If we delete the db, all we need to do is to restart our app
+           // CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try 
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
                 await Seed.SeedUsers(context);
-           }
-           catch(Exception ex)
-           {
-               var logger = services.GetRequiredService<ILogger<Program>>();
-               logger.LogError(ex, "An error occured during migration");
-           }
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex,"An error occurred during migration");
+            }
 
-          await host.RunAsync();
+            await host.RunAsync(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
