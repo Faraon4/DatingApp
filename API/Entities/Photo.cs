ca37b545811567613cfc02ef5e39b135453a3c58
RaduDarii <radu@stud.uni-obuda.hu>
2021-09-30 15:39:26 +0200

    Add Anotation that will name the table in db Photos not Photo and add the new lines of code that will define the correct relation in the db , and as well the correct behavior of db, in case of deleting user , we will delete the photos as well, and a photo must be connect to one user

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
@@ -7,5 +10,11 @@ namespace API.Entities
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
 
+        // We add the new AppUser property in order to inform 
+        // this class where to add photos
+        // And this is know as furly defining the relationship between 2 entities
+        // In our case is the AppUser and Photo
+        public AppUser AppUser { get; set; }
+        public int AppUserId {get;set;} 
     }
 }
