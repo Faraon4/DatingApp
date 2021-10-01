26fa99b28956558f78f0983c70a1ccdc26e67ad0
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-01 20:07:48 +0200

    Use the new methods with members instead of methods with user, because this are more effective

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -28,6 +28,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
+             // Using Getmembers instead of getUsers
             var users = await _userRepository.GetMembersAsynnc();
             return Ok(users);
 
@@ -39,6 +40,7 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
+            // Using Getmembers instead of getUsers
             return await _userRepository.GetMemberAsync(username);
            
         }
