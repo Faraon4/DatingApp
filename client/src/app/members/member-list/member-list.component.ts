6a8553fe6e57b0bd500a9c6cdd5231d8bd8cc188
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-05 00:16:28 +0200

    We declare a variable of type Member which is an array Member[] . and then we create method to load all members in this array

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  members: Member[] = [];
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+    loadMembers() {
+      //subscribe because it is observable, and members => is the response that we get and in the {we specify what we want to di with this}
+      this.memberService.getMembers().subscribe(members => {
+        this.members = members;
+      })
+    }
 }
