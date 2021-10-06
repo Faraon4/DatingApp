bbd1ea7f02ab5ce6036edb63c08e9bbfbd30b5c4
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-07 00:01:21 +0200

    Make method to update the user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -43,6 +44,23 @@ namespace API.Controllers
             // Using Getmembers instead of getUsers
             return await _userRepository.GetMemberAsync(username);
            
+        }
+
+
+        [HttpPut] // This how we update something in our app
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // this should give the username from the token
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
