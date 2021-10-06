bb68ed7778a070a39c7d8fb42070ed11065aa9b4
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-07 00:53:16 +0200

    Make to take the users from the service, not from the new api call

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -9,17 +10,22 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberListComponent implements OnInit {
 
-  members: Member[] = [];
+  members$!: Observable<Member[]>;
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+   // this.loadMembers();
+   this.members$ = this.memberService.getMembers();
   }
 
+  //We do not need this more, because we will take it from our service, but in our service we implemenet , that we don't make an API call all the time
+  /*
     loadMembers() {
       //subscribe because it is observable, and members => is the response that we get and in the {we specify what we want to di with this}
       this.memberService.getMembers().subscribe(members => {
         this.members = members;
       })
     }
+
+    */
 }
