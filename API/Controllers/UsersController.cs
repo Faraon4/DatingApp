5c4d23e72fa92bf6f1f5b8a499db45fd1bf21a33
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-07 13:14:50 +0200

    Create method to upload the photo

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,8 +20,10 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
+            _photoService = photoService;
             _mapper = mapper;
             _userRepository = userRepository;
 
@@ -29,7 +33,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-             // Using Getmembers instead of getUsers
+            // Using Getmembers instead of getUsers
             var users = await _userRepository.GetMembersAsynnc();
             return Ok(users);
 
@@ -43,24 +47,60 @@ namespace API.Controllers
         {
             // Using Getmembers instead of getUsers
             return await _userRepository.GetMemberAsync(username);
-           
+
         }
 
 
         [HttpPut] // This how we update something in our app
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // this should give the username from the token
-            var user = await _userRepository.GetUserByUsernameAsync(username);
+            // Instead of using the next line of code , we add a new extension method that will do this for use
+            // because we need the funcitonality of next line of code in more methods
+            // var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // this should give the username from the token
+            // var username = User.GetUsername();
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
             _mapper.Map(memberUpdateDto, user);
 
             _userRepository.Update(user);
 
 
-            if(await _userRepository.SaveAllAsync()) return NoContent();
+            if (await _userRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update user");
+        }
+
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            // We need to take our user
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) 
+            {
+                return BadRequest(result.Error.Message);
+            }
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0)
+            {
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+
+            if( await _userRepository.SaveAllAsync())
+            return _mapper.Map<PhotoDto>(photo);
+
+            return BadRequest("Problem adding photo");
         }
     }
 }
