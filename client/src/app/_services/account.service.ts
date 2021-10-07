d927760cefdd240698125d7ab3c231a8254cc324
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-07 14:29:06 +0200

    Change a bit the account service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,8 +26,7 @@ currentUser$ = this.currentUserSource.asObservable(); // Observable objects , by
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -37,14 +36,15 @@ currentUser$ = this.currentUserSource.asObservable(); // Observable objects , by
     return this.http.post<User>(this.baseUrl+'account/register', model).pipe(
       map((user: User) => {
         if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+         // localStorage.setItem('user', JSON.stringify(user));
+          this.setCurrentUser(user);
         }
       })
     )
   }
 
 setCurrentUser(user : User) {
+  localStorage.setItem('user', JSON.stringify(user));
   this.currentUserSource.next(user);
 }
 
