336c8a2f4d42dd360b3f15a7831c301c889c2a96
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-11 02:49:23 +0200

    Create the ts file to get access to the control

--- client/src/app/_forms/text-input/text-input.component.ts
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.css']
 })
-export class TextInputComponent implements OnInit {
+export class TextInputComponent implements ControlValueAccessor {
 
-  constructor() { }
+  // we want to implement here a control value accessor
+  // If we look in the register.component.ts, we have there FormControls like username, password and cofrim password
+  // And in this file we want to get access to them
 
-  ngOnInit(): void {
+
+  @Input() label!: string;
+  @Input() type = 'text'; // this is default, and if we want to overide we pass to control
+
+
+  // In constructor we inject the control in the ControlValueAccessor
+  constructor(@Self() public ngControl: NgControl) {
+    // by doing this , we get access to out control
+    this.ngControl.valueAccessor = this;
+   }
+  writeValue(obj: any): void {
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
   }
 
 }
