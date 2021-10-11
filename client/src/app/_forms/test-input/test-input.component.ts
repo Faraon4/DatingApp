8824f2741df73c33c54fd29f283fa6d0ff88f513
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-11 02:18:35 +0200

    Here,we want to implement a control value accessor

--- client/src/app/_forms/test-input/test-input.component.ts
+++ client/src/app/_forms/test-input/test-input.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestInputComponent implements OnInit {
 
+  // We want to implement a control value accessor
   constructor() { }
 
   ngOnInit(): void {
