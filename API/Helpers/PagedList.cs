81213643135f50e5970249570bbff1fee3611751
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-15 02:51:15 +0200

    Add the functionality to the new class, more info in comments

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -2,11 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Helpers
 {
     public class PagedList<T> : List<T>
     {
-        
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize); // If we get a count of 10, and the pageSize is 5 than means that we have 2 pages
+            PageSize = pageSize;
+            TotalCount = count;
+
+            // Range of the items iside constructor so to have access to items inside this list
+            AddRange(items);
+
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; } // How many items are in this query
+
+        // We create a static method that we can call from everywhere
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync(); // Make a db call , and count how many T we have, (T can be everything users messages etc)
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
