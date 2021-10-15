22a64838779c24458e370cdc1b0867fa12e4e493
RaduDarii <radu@stud.uni-obuda.hu>
2021-10-15 03:10:20 +0200

    Make this class to specify the most amount of things that a request can return and add the necesary functionalities, Read the Comments for more inpormation

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class UserParams
+    {
+        // Here we need to speficy the most amount of things that a request can return
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1; // We will take the oage number from the user
+
+        private int _pageSize = 10; //  This is our default size;
+
+
+        public int PageSize 
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; // we make the comparison here, if the pageSize  is greate or less that MaxPageSize
+        }
+    }
+}
