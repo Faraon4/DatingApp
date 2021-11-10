f84b4a675e0e74fd76b78672a76da871ecdff977
RaduDarii <radu@stud.uni-obuda.hu>
2021-11-10 15:09:57 +0100

    Create the controller for the Message and create a method for Posting the messages

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+        public MessagesController(IUserRepository userRepository,
+        IMessageRepository messageRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _messageRepository = messageRepository;
+            _userRepository = userRepository;
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var username = User.GetUsername();
+        
+            // Check if username = to recipinet name to createMessDto
+            if (username == createMessageDto.RecipientUsername.ToLower())
+            {
+                return BadRequest("You cannot send messages to yourself");
+            }
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username); 
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            // if do not have recipient
+
+            if(recipient == null) return NotFound(); 
+
+            // If we get to this stage, we are ready to create a new message
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+                _messageRepository.AddMessage(message);
+
+                if(await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+        
+            return BadRequest("Failed to send message");
+        }   
+    }
+}
