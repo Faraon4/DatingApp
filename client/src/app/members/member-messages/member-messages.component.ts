143b653e390bf46bc8d04da44d1dc1ef091526b6
RaduDarii <radu@stud.uni-obuda.hu>
2021-11-12 16:55:04 +0100

    Delete from the method and add to the other that will help to get method

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -8,21 +8,15 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-  @Input() username!:string;
+  
+  @Input() messages!: Message[];
 
-  messages!: Message[];
-
-  constructor(private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.loadMessages();
   }
 
 
-  loadMessages(){
-    this.messageService.getMessageThread(this.username).subscribe(messages => {
-      this.messages = messages;
-    })
-  }
+
 
 }
