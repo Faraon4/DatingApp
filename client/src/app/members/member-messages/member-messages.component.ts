1863eea3efb9e8934da95a13a0d55feeacd4d1dc
RaduDarii <radu@stud.uni-obuda.hu>
2021-11-13 20:29:44 +0100

    Add the possiblity to send message, by activating the form and activating the possibility to send messages, and as well for more information read the cooments

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
+import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
@@ -8,12 +10,31 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-  
-  @Input() messages!: Message[];
+  // We need to clear the form after we send the message
+  // and for this we will use the @ViewChild
+  @ViewChild('messageForm') meesageForm!: NgForm;
 
-  constructor() { }
+
+  @Input() messages!: Message[];
+  @Input() username!:string; // we get this from member-detail compoent;
+  messageContent!: string;
+
+  constructor(private messageSevice: MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  sendMessage(){
+    this.messageSevice.sendMessage(this.username, this.messageContent).subscribe(message => {
+      this.messages.push(message);
+
+      // when we subscribe, we write (message => because we know that we
+      // will get back the message
+
+
+      // This line of code reset the form 
+      this.meesageForm.reset();
+    })
   }
 
 
