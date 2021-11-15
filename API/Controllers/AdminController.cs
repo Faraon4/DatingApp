b9da62bb2657af52f62bf0f6663f742ae76a69b9
RaduDarii <radu@stud.uni-obuda.hu>
2021-11-15 15:51:24 +0100

    Create method that gives admin posibility to change the roles of members

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -37,6 +37,29 @@ namespace API.Controllers
                                 return Ok(users);
         }
 
+        // Method that give ability to admin to change the roles f the users
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string  roles)
+        {
+            var selectedRoles = roles.Split(",").ToArray(); // we ge this from query string
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null) return NotFound("Could not find such user");
+
+            var userRoles = await _userManager.GetRolesAsync(user); // gives the role of this particular user
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if(!result.Succeeded) return BadRequest("Failed tp add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded) return BadRequest("Failed to removefrom roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+
+        }
 
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photos-to-moderate")]
