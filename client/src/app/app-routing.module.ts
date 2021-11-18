abc6696fa0201f6f1e67e6185d3f85875da7bde7
RaduDarii <radu@stud.uni-obuda.hu>
2021-11-18 15:42:48 +0100

    Add the routingLink for the admin Panel, and adding in the module for the component new

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
@@ -20,11 +21,12 @@ const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]}, // We add a route for our members
-      {path: 'members/:username', component: MemberDetailComponent, resolve: {member: MemberDetailedResolver}}, // we add route for the details of the our members , and for going to details page we will write the id of person
+  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]}, // We add a route for our members
+  {path: 'members/:username', component: MemberDetailComponent, resolve: {member: MemberDetailedResolver}}, // we add route for the details of the our members , and for going to details page we will write the id of person
   {path: 'member/edit', component:MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
   {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent}
+  {path: 'messages', component: MessagesComponent},
+  {path: 'admin', component: AdminPanelComponent}
     ]
   },
   // Route for the errors
