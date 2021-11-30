d39250cc6c228efa234d5d1fd0c2dfbfd52b5091
RaduDarii <radu@stud.uni-obuda.hu>
2021-11-30 21:18:17 +0100

    Start to create the modal corect for our app

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-roles-modal',
@@ -10,10 +11,15 @@ export class RolesModalComponent implements OnInit {
 
   constructor(public bsModalRef: BsModalRef) { }
 
-  title!: string;
-  list :any[] = [];
-  closeBtnName!: string;
+  //receive from component
+  @Input() updateSelectedRoles = new EventEmitter();
+  user!: User;
+  roles!: any[];
   ngOnInit(): void {
   }
 
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
+  }
 }
